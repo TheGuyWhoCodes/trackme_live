47854daaea6794df4f2e94e1f375d449b4b30018
noah mohabbat <noahrmohabbat@gmail.com>
2021-11-12 19:50:43 -0600

    joystick

--- trackme-webui/src/app/app.component.ts
+++ trackme-webui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { create as nipplejsCreate } from 'nipplejs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trackme-webui';
+
+  ngOnInit() {
+      let options = {
+          zone: document.getElementById('zone_joystick'),
+          color: "red",
+          mode: "static" as "dynamic" | "semi" | "static",
+          position: {left: "50%", bottom: "50%" }
+      }
+      let manager = nipplejsCreate(options);
+  }
 }
