534fe333de2854718a92c85d78cea2c71c665a17
Christopher Ariagno <christopherariagno@gmail.com>
2022-01-19 17:13:31 -0600

    server.py changes related to logging and camera init

--- server/server.py
+++ server/server.py
@@ -5,58 +5,54 @@ from visca import camera
 import json
 from time import sleep
 
-app = Flask(__name__,
-            static_folder='static',
-            template_folder='templates')
+# Init Flask Instances
+app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
-cam = camera.D100(output='COM4')
-cam.init()
-cam.home()
 
-@app.route('/')
-def index():
-    """Home page."""
-    return render_template('index.html')
+# Init Camera object (Default COM4). This probably won't connect
+# and most likely we will need to not initalize it until we see what the user
+# chooses, but for right now this is it. Maybe if we add more cameras, we
+# could add some way to track multiple camera d100 objects ie: hashmap? idk spitballing
+try:
+	cam = camera.D100(output='COM4')
+	cam.init()
+	cam.home()
+except:
+	cam = None
+	app.logger.warning("Unable to create camera device, wait for the user to do it.")
 
 @socketio.on('connect')
 def connect_web():
     print('[INFO] Web client connected: {}'.format(request.sid))
 
-@socketio.on('test')
-def test():
-    print('[INFO] Web client {} said TEST'.format(request.sid))
-
 @socketio.on('disconnect')
 def disconnect_web():
     print('[INFO] Web client disconnected: {}'.format(request.sid))
 
-
+# Handle change in direction of camera, this will need to be
+# swapped to a case statement with enums if doable
 @socketio.on('change_state')
 def handle_state(message):
 	print('[INFO] Web client {}: Command = change_state => {}'.format(request.sid, message))
-	# parsed = json.loads(message)
+	if cam == None:
+		print("HITTING")
+		socketio.emit('server2web',{ 'text':'Unable to move camera, not initalized'}, namespace='/web')
+		return
 	if(message['direction'] == "up"):
 		cam.up(amount=10)
-		# sleep(1)
-		# cam.stop()
+		cam.stop()
 	elif(message['direction'] == "down"):
 		cam.down(amount=10)
-		# sleep(1)
-		# cam.stop()	
+		cam.stop()	
 	elif(message['direction'] == "left"):
 		cam.left(amount=10)
-		# sleep(1)
-		# cam.stop()	
+		cam.stop()	
 	elif(message['direction'] == "right"):
 		cam.right(amount=10)
-		# sleep(1)
-		# cam.stop()
+		cam.stop()
 	elif(message['direction'] == "stop"):
 		cam.stop()	
 	socketio.emit('server2web',{ 'text':'Command = change_state => {}'.format(message)}, namespace='/web')
-
-#def new_frame():
-
 
 if __name__ == "__main__":
     print('[INFO] Starting server at http://localhost:4001')
