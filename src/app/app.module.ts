15ddde3534f42bddb7e69448410b38f4231db161
noah mohabbat <noahrmohabbat@gmail.com>
2022-02-09 13:39:42 -0600

    video feed, ui is better, but it is scrambled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { WebcamModule } from 'ngx-webcam';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 const socketConfig = {
 	url: 'http://localhost:4001', // Where the Socket IO instance is for Flask
@@ -17,7 +21,10 @@ const socketConfig = {
   imports: [
     BrowserModule,
 	AppRoutingModule,
-	SocketIoModule.forRoot(socketConfig)
+    WebcamModule,
+    MatSliderModule,
+	SocketIoModule.forRoot(socketConfig),
+	BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
