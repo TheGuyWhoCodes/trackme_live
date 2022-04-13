134d72233af5173e7bfb775c5660cb9c3f0a60e5
nmrnm <noahrmohabbat@gmail.com>
2022-04-12 21:46:36 -0500

    skd

--- server/server.py
+++ server/server.py
@@ -13,7 +13,7 @@ import asyncio
 import platform
 import subprocess
 if platform.system() == 'Windows':
-	import winrt.windows.devices.enumeration as windows_devices
+	import winsdk.windows.devices.enumeration as windows_devices
 
 from threading import Thread
 	
